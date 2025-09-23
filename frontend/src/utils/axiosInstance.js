847f0afa7e12146ceb9d25681af52f60acd72ffa
shubhamchand84 <shubhamchandofficial@gmail.com>
2025-09-23 11:24:58 +0530

    in frontend backend url pasted

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,13 +1,30 @@
-import axios from "axios"
+// import axios from "axios"
 
-const BASE_URL = "http://localhost:5000/api"
+// const BASE_URL = "http://localhost:5000/api"
+
+// const axiosInstance = axios.create({
+//   baseURL: BASE_URL,
+//   withCredentials: true,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// })
+
+// export default axiosInstance
+
+
+
+import axios from "axios";
+
+// Deployed backend URL
+const BASE_URL = "https://travelsnapbackend.onrender.com/api";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true,
+  withCredentials: true, // keep this if your backend uses cookies/auth
   headers: {
     "Content-Type": "application/json",
   },
-})
+});
 
-export default axiosInstance
+export default axiosInstance;
