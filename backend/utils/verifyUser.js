335533f4d8ba867c61dcfa640f6a81c0a28d43e1
shubhamchand84 <shubhamchandofficial@gmail.com>
2025-09-23 17:49:13 +0530

    verifyuse.js fix

--- backend/utils/verifyUser.js
+++ backend/utils/verifyUser.js
@@ -24,7 +24,8 @@ import jwt from "jsonwebtoken";
 import { errorHandler } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.token; // <<< changed from access_token
+  // Check for token in both possible cookie names for compatibility
+  const token = req.cookies.token || req.cookies.access_token;
 
   if (!token) {
     return next(errorHandler(401, "Unauthorized"));
